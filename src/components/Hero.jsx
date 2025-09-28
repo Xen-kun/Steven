08d0d66cb20f8ec0d7433873732f635a2c076a13
Xen-kun <stevedaleja2001@gmail.com>
2025-09-28 11:04:15 +0800

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section
-      id="hero"
+      id="home"
       className="min-h-screen flex flex-col items-center justify-center text-center px-4 bg-white"
     >
       {/* Title */}
