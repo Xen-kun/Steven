585108a8eb6e7512a720e454d14cc6ec817c8ca4
Xen-kun <stevedaleja2001@gmail.com>
2025-10-23 11:12:08 +0800

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@
 export default function Footer(){
     return (
         <p className="text-sm text-gray-500">
-          © {new Date().getFullYear()} Steven.
+          © {new Date().getFullYear()} Steven
         </p>
     )
 }
